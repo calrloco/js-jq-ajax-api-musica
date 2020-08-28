36701ac62b4956a3ce9e1fd0ad5c74a092321f22
carlo briganti <505641nl@saeinstitute.edu>
2020-08-28 15:06:38 +0200

    da

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
-	//Code
+$(document).ready(function () {
+    $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function (data) {
+	  var response = data.response;
+	  var source = $('.template').html();
+	  var html = teplate()
+	  for (var i=0;i<response.length;i++){
+         $('.container-cd').append('ciao');
+	  }
+	},
+    error: function () {
+      alert("errore impossibile caricare");
+    },
+  });
+  
 });
